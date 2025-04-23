fa6adbfc41ba4906b813ddb025ed4d1eded51a53
Fernando <vluisfernando62@gmail.com>
2025-04-22 21:21:13 -0400

    ✨ feat(layout): agregar el componente layout ccon los estilos de flowbite

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+
+        <Navbar />
+        <Sidebar />
+
+        <slot />
+
+    </div>
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+import Navbar from './Layouts/Navbar.vue';
+import Sidebar from './Layouts/Sidebar.vue';
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
