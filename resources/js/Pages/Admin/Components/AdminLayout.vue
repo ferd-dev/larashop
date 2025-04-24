5582461e590569b4293dbb14d65f3240c3647ed1
Fernando <vluisfernando62@gmail.com>
2025-04-24 17:58:08 -0400

    🐛 fix(admin-layout): agregar una clase para envolver el slot

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -1,23 +1,22 @@
 <template>
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
-
         <Navbar />
         <Sidebar />
 
-        <slot />
-
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            <slot />
+        </main>
     </div>
 </template>
 
 <script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
 
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
-
-import Navbar from './Layouts/Navbar.vue';
-import Sidebar from './Layouts/Sidebar.vue';
+import Navbar from "./Layouts/Navbar.vue";
+import Sidebar from "./Layouts/Sidebar.vue";
 
 onMounted(() => {
     initFlowbite();
-})
+});
 </script>
