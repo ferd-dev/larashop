5737f7cda312b35dcd3cbecebe070c1517e60800
Fernando <vluisfernando62@gmail.com>
2025-04-24 18:17:21 -0400

    ✨ feat(product-controller): implementar el método de guardado de productos y carga de imágenes de productos

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -16,5 +18,32 @@ class ProductController extends Controller
         return Inertia::render('Admin/Products/Index', [
             'products' => $products,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->save();
+
+        if ($request->hasFile('product_images')) {
+            $productImages = $request->file('product_images');
+            foreach ($productImages as $image) {
+                $uniqueImageName = time() . '-' . Str::ramdom(10) . '.' . $image->getClientOriginalExtension();
+                $image->move('product_images', $uniqueImageName);
+
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueImageName,
+                ]);
+            }
+        }
+
+        return redirect()->route('admin.products.index')->with('success', 'Product created successfully.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'redirectAdmin'], function ()
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
     Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
+    Route::get('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
 });
 
 Route::get('/home', function () {
