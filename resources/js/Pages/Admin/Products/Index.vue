471a920831443563e6224def1567a7be2688f1b4
Fernando <vluisfernando62@gmail.com>
2025-04-24 17:58:58 -0400

    ✨ feat(products): agregar los componentes ProductList e Index para la gestión de productos

---
+++ resources/js/Pages/Admin/Products/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AdminLayout from "../Components/AdminLayout.vue";
+import ProductList from "./ProductList.vue";
+</script>
+<template>
+    <AdminLayout>
+        <ProductList />
+    </AdminLayout>
+</template>
