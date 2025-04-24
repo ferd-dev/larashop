872e8fde880f989bba5ec396fcb1f64b2fb45bd7
Fernando <vluisfernando62@gmail.com>
2025-04-24 19:09:15 -0400

    ✨ feat(app): Importar el CSS  de ElementPlus para componentes UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,9 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createApp, h } from "vue";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+
 import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
