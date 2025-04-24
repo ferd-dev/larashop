c0c43a811a2d81111830ecd95e6ba0c3c2b89ca3
Fernando <vluisfernando62@gmail.com>
2025-04-24 12:44:23 -0400

    ✨ feat(seeder): agregar AdminSeeder
    - crear un usuario administrador por defecto

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@larashop',
+            'password' => bcrypt('12345678'),
+            'is_admin' => 1,
+        ]);
+    }
+}
