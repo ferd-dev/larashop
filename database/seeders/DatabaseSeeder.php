3944199d9403f006f33660f82df057a76ca75809
Fernando <vluisfernando62@gmail.com>
2025-04-24 15:04:26 -0400

    ✨ feat(seeder): agregar factories para Category, Brand y Product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Category::factory(10)->create();
+        Brand::factory(10)->create();
+        Product::factory(100)->create();
     }
 }
