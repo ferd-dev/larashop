802abcca2cf04cf772ed7104692a61ad4b804d13
Fernando <vluisfernando62@gmail.com>
2025-04-24 20:14:57 -0400

    🐛 fix(routes): cambiar el método de ruta del guardado de productos de GET a POST

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 
     Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
-    Route::get('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
 });
 
 Route::get('/home', function () {
