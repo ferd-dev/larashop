9487d91979858e7ac6e1d970b665a44ffdc7ec41
Fernando <vluisfernando62@gmail.com>
2025-04-26 21:51:37 -0400

    ✨ feat(product-controller): agregar funcionalidad para actualizar producto y eliminar imágenes de productos

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category', 'brand')->paginate(10);
+        $products = Product::with('category', 'brand', 'product_images')->paginate(10);
         $brands = Brand::all();
         $categories = Category::all();
 
@@ -52,5 +52,38 @@ class ProductController extends Controller
         }
 
         return redirect()->route('admin.products.index')->with('success', 'Product created successfully.');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        if ($request->hasFile('product_images')) {
+            $productImages = $request->file('product_images');
+            foreach ($productImages as $image) {
+                $uniqueImageName = time() . '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
+                $image->storeAs('product_images', $uniqueImageName, 'public');
+
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueImageName,
+                ]);
+            }
+        }
+
+        $product->update();
+        return redirect()->route('admin.products.index')->with('success', 'Product updated successfully.');
+    }
+
+    public function deleteImage($id)
+    {
+        ProductImage::where('id', $id)->delete();
+        return redirect()->route('admin.products.index')->with('success', 'Imagen product deleted successfully.');
     }
 }
