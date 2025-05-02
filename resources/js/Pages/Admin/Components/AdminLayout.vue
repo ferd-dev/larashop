714a6b2af3e377b62ed4120d1434dd46fddd064c
Fernando <vluisfernando62@gmail.com>
2025-05-02 19:23:28 -0400

    ♻️ refactor(layout): crear el componente Footer y agregarlo al AdminLayout

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <div class="antialiased bg-gray-50">
         <Navbar />
         <Sidebar />
 
         <main class="p-4 md:ml-64 h-auto pt-20">
             <slot />
+            <Footer />
         </main>
     </div>
 </template>
@@ -15,6 +16,7 @@ import { initFlowbite } from "flowbite";
 
 import Navbar from "./Layouts/Navbar.vue";
 import Sidebar from "./Layouts/Sidebar.vue";
+import Footer from "./Layouts/Footer.vue";
 
 onMounted(() => {
     initFlowbite();
---
+++ resources/js/Pages/Admin/Components/Layouts/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+<footer class="bg-slate-600 text-white rounded-lg shadow-sm">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="text-sm text-white sm:text-center">
+            © 2025
+            <a href="https://flowbite.com/" class="hover:underline">Ferchs</a>
+            . Todos los derechos reservados.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+            <li>
+                V.  1.0.0
+            </li>
+        </ul>
+    </div>
+</footer>
+</template>
